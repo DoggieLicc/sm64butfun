0758fe17d0dbfc120555439b7d41f83bce9339df
DoggieLicc <76987398+DoggieLicc@users.noreply.github.com>
2023-06-29 19:10:48 -0400

    h

--- src/game/mario_actions_airborne.c
+++ src/game/mario_actions_airborne.c
@@ -902,7 +902,7 @@ s32 act_ground_pound(struct MarioState *m) {
 
             if (m->input & INPUT_A_PRESSED) {
                 set_mario_action(m, ACT_JUMP, 0);
-                m->vel[1] += 20.0f
+                m->vel[1] += 20.0f;
             }
 
             set_camera_shake_from_hit(SHAKE_GROUND_POUND);
